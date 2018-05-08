fec8d5293d9bcd43e88dab88c05743e57cb6eec9
Joseph Gonzalez <josegonzalez2330@gmail.com>
2018-05-07 22:51:58 -0600

    Se agrega la libreria y se realizan pruebas con el server

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
+import Socket.Server;
+
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        Server server = Server.getServerInstance();
+        while (server.getClient().isConnected()){
+            server.listen();
+        }
+
 
     }
 }
--- src/Socket/Server.java
+++ src/Socket/Server.java
@@ -1,12 +1,18 @@
 package Socket;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
+import java.net.Socket;
+
+import com.fasterxml.jackson.dataformat.xml.JacksonXmlModule;
 // la clase utiliza un singleton para que solo haya un server en cualquier momento
 public class Server {
 
     private static Server serverInstance;
     private ServerSocket serverSocket;
+    private Socket client;
 
     /**
      * Metodo para obtener la instancia de server, es un singleton
@@ -25,15 +31,26 @@ public class Server {
         try {
             // Se inicia el server en el puerto 3000
             this.serverSocket = new ServerSocket(3000);
+            this.client = serverSocket.accept();
 
         }catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void send(){
+    public void send() {
+    }
+    public void listen() throws IOException {
+        String data;
+        BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        while ((data = in.readLine()) != null){
+            System.out.println(data);
+        }
+
 
     }
 
-
+    public Socket getClient() {
+        return client;
+    }
 }
